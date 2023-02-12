56b2b7480fbb2a0dd5d35d19698f6f2b7fb9a86b
Lucia <luciamouriz91@gmail.com>
2023-02-12 17:53:42 +0100

    Accion de UIDate - botones

--- src/components/UIDate/UIDate.jsx
+++ src/components/UIDate/UIDate.jsx
@@ -1,18 +1,20 @@
-import { useRef, useState } from "react"
+import { useState } from "react"
 import { Calendar } from "../calendar/grid/Calendar"
 
-
 export const UIDate = () => {
+
 
   let today = new Date();
   let monthToday = today.getMonth();
   let yearToday = today.getFullYear();
+  const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
 
-  const [showComponent, setShowComponent] = useState(false);
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [showUIDate, setShowUIDate] = useState(false);
   const [inputValue, setInputValue] = useState([]);
   const [id, setID] = useState('');
-  const myInput = useRef(null);
+ 
 
   const handleInputChange = event => {
     setInputValue(event.target.value);
@@ -21,36 +23,36 @@ export const UIDate = () => {
   const handleClickMonth = event => {
     /* Array.from(document.getElementsByTagName('button')).map(elem => elem.classList.remove('active'))
     event.currentTarget.classList.add('active'); */
-    setID(event.currentTarget.id)
-    setShowComponent(true)
+
+    if (inputValue.length === 0) {
+      alert("falta el año")
+    } else {
+      setID(event.currentTarget.id)
+      setShowCalendar(true)
+      setShowUIDate(false)
+    }
   }
 
-  const getMonthText = () =>{
-    
+  const handleClickUIDate = event => {
+    setShowUIDate(true)
   }
+
 
   return (
     <>
-      <p>CALENDARIO / <button>{(id != "")  ? `${id}` : `${monthToday}`}</button></p>
-      <div className="uidate-wrapper" >
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button id={0} onClick={handleClickMonth}>Enero</button>
-        <button id={1} onClick={handleClickMonth}>Febrero</button>
-        <button id={2} onClick={handleClickMonth}>Marzo</button>
-        <button id={3} onClick={handleClickMonth}>Abril</button>
-        <button id={4} onClick={handleClickMonth}>Mayo</button>
-        <button id={5} onClick={handleClickMonth}>Junio</button>
-        <button id={6} onClick={handleClickMonth}>Julio</button>
-        <button id={7} onClick={handleClickMonth}>Agosto</button>
-        <button id={8} onClick={handleClickMonth}>Septiembre</button>
-        <button id={9} onClick={handleClickMonth}>Octubre</button>
-        <button id={10} onClick={handleClickMonth}>Noviembre</button>
-        <button id={11} onClick={handleClickMonth}>Diciembre</button>
-        {showComponent == false && <Calendar month={monthToday} year={yearToday} />}
-        {showComponent && <Calendar month={id} year={inputValue} />}
-      </div>
+      <p>CALENDARIO / <button onClick={handleClickUIDate}>{id != "" ? months[id] : months[monthToday]}</button></p>
 
+      {showUIDate &&
+        <div className="uidate-wrapper">
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+          {months.map((month, index) => (
+            <button id={index} onClick={handleClickMonth}>{month}</button>
+          ))}
 
+        </div>
+      }
+
+      {showCalendar == true && inputValue != "" ? <Calendar month={id} year={inputValue} /> : <Calendar month={monthToday} year={yearToday} />}
     </>
 
   )
